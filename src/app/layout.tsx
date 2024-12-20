c53c45eee0dfb3bd5ab899f1fa8575d4e67791ac
covelitein <abrahamcovenant2004@gmail.com>
2024-12-20 17:11:19 +0100

    almost done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,45 +12,9 @@ const montserrat = localFont({
 });
 
 export const metadata: Metadata = {
-  metadataBase: new URL(
-    process.env.APP_URL
-      ? `${process.env.APP_URL}`
-      : process.env.VERCEL_URL
-      ? `https://${
-          process.env.VERCEL_URL || "covelitein-hub.vercel.app"
-        }`
-      : `http://localhost:${process.env.PORT || 3000}`
-  ),
-  title: "CoveliteinsHub - Eccomerce demo project",
+  title: "Dynamics365",
   description:
-    "CoveliteHub provides state-of-the-art solutions for businesses, empowering innovation and growth with cutting-edge technology and seamless user experiences.",
-  alternates: {
-    canonical: "/",
-  },
-  openGraph: {
-    url: "/",
-    title: "CoveliteinsHub - Empowering Businesses",
-    description:
-      "Explore CoveliteinsHub's innovative solutions to transform your business. Seamlessly crafted for desktop and mobile platforms.",
-    type: "website",
-    images: [
-      {
-        url: "https://scontent.flos1-2.fna.fbcdn.net/v/t39.30808-6/469693135_1370358640609131_8794368040634332744_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeHgvAXBI3hQdmHZfvG9lYCQSCamk_Uh4JRIJqaT9SHglCa-oy0zXj7JEgJ5oTNTSn3l2To9mY8Mf8zevjaHwdBK&_nc_ohc=t-2zpAra09QQ7kNvgGD9hSc&_nc_zt=23&_nc_ht=scontent.flos1-2.fna&_nc_gid=AvELAL3D3SZ_fS88fYxK2dB&oh=00_AYBNovQDQe45eareFpLo0ZX4d2cQFlCZs3qjSvZoZVku5Q&oe=675E5828",
-        width: 1200,
-        height: 630,
-        alt: "CoveliteinsHub - Empowering Businesses",
-      },
-    ],
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "CoveliteinsHub - Your Gateway to Innovation",
-    description:
-      "Discover how CoveliteinsHub empowers businesses with innovative solutions, cutting-edge technology, and seamless user experiences.",
-    images: [
-      "https://scontent.flos1-2.fna.fbcdn.net/v/t39.30808-6/469693135_1370358640609131_8794368040634332744_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeHgvAXBI3hQdmHZfvG9lYCQSCamk_Uh4JRIJqaT9SHglCa-oy0zXj7JEgJ5oTNTSn3l2To9mY8Mf8zevjaHwdBK&_nc_ohc=t-2zpAra09QQ7kNvgGD9hSc&_nc_zt=23&_nc_ht=scontent.flos1-2.fna&_nc_gid=AvELAL3D3SZ_fS88fYxK2dB&oh=00_AYBNovQDQe45eareFpLo0ZX4d2cQFlCZs3qjSvZoZVku5Q&oe=675E5828",
-    ],
-  },
+    "Dynamics365 project.",
 };
 
 export default function RootLayout({
